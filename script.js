7bd237940b337c182c3b3f80cd8a219a2768131c
Brenda Beick <brendadbeick@gmail.com>
2022-09-28 15:21:18 -0300

    arrumando o script

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ const textoprincipal = document.querySelector("#texto-principal");
 const textosecundario = document.querySelector("#texto-secundario");
 
 function btncriptografar() {
-    const textocriptografado = cripto(textoprincipal.value);
-    textosecundario.value = textocriptografado;
+    const textocriptogrado = cripto(textoprincipal.value);
+    textosecundario.value = textocriptogrado;
 }
 
 function cripto (stringcriptografada) {
@@ -18,24 +18,24 @@ function cripto (stringcriptografada) {
     return stringcriptografada;
 }
 
-function btndescriptografar () {
-    const textodescriptografado = descripto(textoprincipal.value);
-    textosecundario.value = textodescriptografado;
+function btndescriptografar() {
+    const textodescriptogrado = descripto(textoprincipal.value);
+    textosecundario.value = textodescriptogrado;
 }
 
 function descripto (stringdescriptografada) {
     let matrizcodigo = [["a","ai"],["e","enter"],["i","imes"],["o","ober"],["u","ufat"]];
     stringdescriptografada = stringdescriptografada.toLowerCase();
 
-    for(let i = 0; i < matrizcodigo.length;i++) {
+    for(let i = 0; i< matrizcodigo.length;i++) {
         if(stringdescriptografada.includes(matrizcodigo[i][1])) {
             stringdescriptografada = stringdescriptografada.replaceAll(matrizcodigo[i][1],matrizcodigo[i][0])
         }
-
+    }
     return stringdescriptografada;
 }
 
- function btntextocopiado () {
+ function btntextocopiado() {
     const copiartexto = document.querySelector("textosecundario");
     const copiado = textosecundario.value;
     navigator.clipboard.writeText(copiado);
